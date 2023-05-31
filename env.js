fa333064594f11442441c3e045442c2078650aba
Ratan Mia <ratan.kawasaki@gmail.com>
2023-05-31 17:29:12 +0600

    NPM installed and AI package added

---
+++ env.js
@@ -0,0 +1,5 @@
+export const process = {
+    env: {
+        OPENAI_API_KEY: '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'
+    }
+}
--- index.js
+++ index.js
@@ -1,15 +1,25 @@
 
-const apiKey = "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";
+import { Configuration, OpenAIApi } from 'openai';
+import { process } from '/env.js';
 
 
 
+// const apiKey = "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";
+
+const apiKey = process.env.OPENAI_API_KEY
 const url ="https://api.openai.com/v1/completions";
 
+const configuration = new Configuration({
+  apiKey:  process.env.OPENAI_API_KEY
+})
 
+const openai = new OpenAIApi(configuration)
 
 const setupTextarea = document.getElementById('setup-textarea') ;
 const setupInputContainer = document.getElementById('setup-input-container');
 const movieBossText = document.getElementById('movie-boss-text');
+
+
 
 document.getElementById("send-btn").addEventListener("click", () => {
   if (setupTextarea.value) {
@@ -30,7 +40,7 @@ function fetchBotReply(){
         },
         body: JSON.stringify({
             'model' : 'text-davinci-003',
-            'prompt': 'WhSound sympathetic in five words or less.'
+            'prompt': 'Sound enthusiastic in five words or less.'
     
     
         })
